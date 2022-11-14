ac706c902868b0f3129cf3c6416bb4df63fa3326
Anzhilia <104268987+Anzhilia@users.noreply.github.com>
2022-11-14 09:40:06 +0800

    Update xjtu-ydc.js

--- xjtu-ydc.js
+++ xjtu-ydc.js
@@ -1,112 +1,167 @@
 // ==UserScript==
-// @name         New Userscript
+// @name         xjtu-ydc-sr
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      0.4
+// @description         xitu-ydc-sr
+// @description:zh-CN   xjtu-ydc-sr
+// @author       AnZhili
 // @match        http://202.117.17.144/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=17.144
 // @grant        none
+// @license      AnZhili
 // ==/UserScript==
-
+ 
 (function() {
-    document.getElementById("reserve").style.display = "block";
-    var u = window.location.href;
-    $(document).click(function(e){
-            var id=e.target.id;
-            console.log(id);
-            if(id=="reserve"){
-            if(u.includes('setNoPrice')){
-            document.getElementById("reserve").attachEvent=("onclick",applySeat());
-            }
-            if(u.includes('order')){
-            document.getElementById("reserve").attachEvent=("onclick",yzmWindow());
-            }
-
-
-
-            }
-    });
-function applySeat() {
-	var num = Number($('.tip-number .red').text())/Number($('#daynum').text());
-
-	if (num > 0) {
-		loadInfo("锁定"+typename+"", "正在为您锁定"+typename+".....请勿刷新页面！");
-		var model = {};
-		var seats = $('span[id^=s_]');
-		var seatid = [];
-		$.each(seats, function(i, item) {
-			seatid.push($(item).attr('id').split('_')[1]);
-		})
-		var stock = {};
-		var stockids = $('#stockid').val().split(',');
-		for(var i = 0;i<stockids.length;i++){
-			stock[stockids[i]] = num+'';
-		}
-		model.stock = stock;
-		model.address = $('#serviceid').val();
-		model.stockdetailids = seatid.join(',');
-//		alert(JSON.stringify(model));
-//		model.stockid = $('#stockid').val();
-//		model.number=num;
-//		AjaxPost('/order/book',param1,createSuccess);
-		if($('#yuding-method').find(".selected").attr('date') == 'day'){
-			var param = {};
-			param.serviceid = $('#serviceid').val();
-			param.num = seatid.length;
-			param.date = $('#s_date').val();
-
-			if($('#s_date').find('.rac').length>0){
-				$.each($('#s_date .selected'), function(i, item) {
-					param.date = $(item).text();
-				});
-			}
-			if ($('#select_date').length>0) {
-				param.date = $('#select_date').datebox('getValue');
-			}
-			var t = $("#extend").find('li');
-			var extend = {};
-			for (var i = 0; i < t.length; i++) {
-				if($('#zbid_'+i).next('input').val()>0){
-					extend[$('#zbid_'+i).val()] = $('#zbid_'+i).next('input').val();
+    try{
+    document.getElementById('showWin').style.display='none';
+    document.getElementById('info').style.display='none';
+    }catch(e){
+ 
+    }
+	var yzm = false;
+	var currentTime = new Date(); //本地时间
+	var currentTimeStr = currentTime.toString();
+	var restString = " 09:06:01 GMT+08:00";
+	var targetTimeStr = currentTimeStr.substring(0, 15) + restString;
+	var targetTime = new Date(targetTimeStr); //目标时间(根据今天的时间自动更新);
+	console.log("targetTime:" + targetTime);
+	//确定预定时间
+    try{
+	document.getElementById("reserve")
+		.style.display = "block";}
+    catch(e){
+    }
+	var u = window.location.href;
+ 
+	//在product页面下的操作；不进行时间预判；
+	if (u.includes('product')) {
+		$(document)
+			.click(function(e) {
+				var id = e.target.id;
+				console.log(id);
+				if (id == "reserve") {
+					document.getElementById("reserve")
+						.attachEvent = ("onclick", applySeat());
 				}
-			}
-			model.extend = extend;
-			//AjaxGet('/order/booklimt',param,function(o){
-			//	if(o.result == 1){
-			//		$('#param').val(JSON.stringify(model));
-			//		$('#form1').submit();
-			//	}else{
-			//		info(o.message);
-			//	}
-			//},'json');
-			$('#param').val(JSON.stringify(model));
-					$('#form1').submit();
-		}else{
-			$('#param').val(JSON.stringify(model));
-			$('#form1').submit();
-		}
-
-
-
-
-
-	} else {
-		info("请选择需要预订的"+typename+"信息！");
+			});
+		//创建时间控件
+		var t = document.createElement("text");
+		t.style.margin = "10px";
+		t.style.width = "100px";
+		t.style.height = "30px";
+		document.body.appendChild(t);
+		var xhsj = setInterval(function() {
+			//循环看时间
+			var time = new Date()
+				.toLocaleTimeString();
+			t.innerHTML = time;
+		}, 1000);
 	}
-
-}
-
-function yzmWindow(){
-	var html = '<div class="typecode" id="typecode">' +
-		'<dl class="dl-input sprite code-filed">' +
-		'<dd><input type="text" txt="验证码" placeholder="验证码" class="textinput" name="yzm" id="yzm"></dd>' +
-		'</dl>' +
-		'<span class="code-img"><img onclick="change();" src="'+ $('#contextPath').val() +'/login/yzm'+ $('#ctrlSuffix').val() +'?'+ Math.random() +'" alt="验证码" height="44" width="108" /></span>'+
-		'</div>'
-	showDialog(600, 300, 169, '填写验证码', true, html, 200);
-	$('.dialog-foot').html("<button class=\"normal-button-mid button-info\" onclick=\"yzmyz();\">&nbsp;&nbsp;继续预订&nbsp;&nbsp;</button>");
-
-}
-
+ 
+	//订购页面下，进行时间预判
+	if (u.includes('order')) {
+		//document.getElementById("reserve").attachEvent=("onclick",yzmWindow());
+		yzmWindow();
+		console.log("yzm");
+		//创建时间控件
+		t = document.createElement("text");
+		t.style.margin = "10px";
+		t.style.width = "100px";
+		t.style.height = "30px";
+		document.body.appendChild(t);
+		var xhsj2 = setInterval(function() {
+			//循环看时间
+			var time = new Date()
+				.toLocaleTimeString();
+			t.innerHTML = time;
+			var restTime = getRestTime(targetTime);
+			console.log(restTime)
+			//这里判断
+			if (restTime < -1000) {
+				//说明超过了40分1秒,人工处理提交了；
+				clearInterval(xhsj2);
+			} else if (-1000<restTime && restTime<1000) {
+				//说明时间在00-01秒之间,这里进行提交
+				document.querySelector("#dlg > div.dialog-foot > button")
+					.click();
+                console.log('时间在0-1秒之内')
+				clearInterval(xhsj2);
+			} else if (1000<restTime && restTime<10000) {
+				//时间还有5-10秒时，进行验证码处理；并判断是否验证码处理；
+				if (yzm == false) {
+					//说明没验证码；
+					var img = document.querySelector("#typecode > span > img"); //应该是图片不可以跨域访问导致的问题
+					var url = downloadIamge(img.src);
+					yzm = true;
+				}
+			}else{
+            console.log('等待中...')
+            }
+			//继续等待
+		}, 1000);
+	}
+ 
+ 
+ 
+ 
+	//获取时间差值
+	function getRestTime(targetTime) {
+		currentTime = new Date();
+		var restTime = targetTime.getTime() - currentTime.getTime();
+		return restTime;
+	}
+ 
+	function downloadIamge(imgsrc) { //下载图片地址和图片名
+		let image = new Image();
+		// 解决跨域 Canvas 污染问题
+		image.setAttribute("crossOrigin", "anonymous");
+		image.src = imgsrc;
+		image.onload = function() {
+			let canvas = document.createElement("canvas");
+			canvas.width = image.width;
+			canvas.height = image.height;
+			let context = canvas.getContext("2d");
+			context.drawImage(image, 0, 0, image.width, image.height);
+			let baseURL = canvas.toDataURL("image/png"); //得到图片的base64编码数
+			var webURL = baseURL.slice(22);
+			document.querySelector("#typecode > span > img")
+				.src = 'data:image/png;base64,' + encodeURIComponent(webURL);
+			//console.log(encodeURIComponent(webURL))
+			imgcode(encodeURIComponent(webURL)) //获取验证码
+		};
+	}
+ 
+ 
+	function imgcode(uri) {
+		//token
+		//uri传进来
+		$.ajax({
+			type: "post",
+			url: "http://www.bhshare.cn/imgcode/",
+			data: {
+				token: '7fcbcaf8c',
+				uri: 'data:image/png;base64,' + uri,
+				type: "online"
+			},
+			dataType: "json",
+			success: function(data) {
+				console.log(data);
+				if (data.code > 0) {
+					console.log(data.data);
+					document.querySelector("#yzm")
+						.value = data.data;
+				} else {
+					console.log(data.msg);
+				}
+			},
+			error: function(result) {
+				console.log(result);
+				console.log(JSON.stringify(result));
+				alert("系统繁忙,请稍后再试！");
+			}
+ 
+		});
+	}
+ 
+ 
 })();
